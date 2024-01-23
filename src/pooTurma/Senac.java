1250531a73d85566aa020c064dd14327fe237028
pamellapereto <pamellapereto@gmail.com>
2024-01-23 10:41:32 -0300

    2.0: criada a classe turma

--- src/pooTurma/Senac.java
+++ src/pooTurma/Senac.java
@@ -1,5 +1,16 @@
 package pooTurma;
 
 public class Senac {
+	
+	public static void main(String[] args) {
+		//Objeto 1
+		
+		
+		
+		
+		//Objeto 2
+		
+		
+	}
 
 }
--- src/pooTurma/Turma.java
+++ src/pooTurma/Turma.java
@@ -1,5 +1,27 @@
 package pooTurma;
 
-public class Turma {
 
+public class Turma {
+	//Atributos da classe
+	String nome;
+	String telefone;
+	String email;
+	
+	//Construtor
+	public Turma() {
+		System.out.println("_______________");
+	}
+	
+	//Métodos
+	void ligarPc() {
+		System.out.println("Computador ligado");
+	}
+	
+	void logar() {
+		System.out.println("Login feito com sucesso");
+	}
+		
+	void desligarPc() {
+		System.out.println("Computador desligado");
+	}
 }
